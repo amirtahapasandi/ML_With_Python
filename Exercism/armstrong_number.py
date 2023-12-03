680b47ce2e3e1fa96faa810940f45a731340582f
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2023-12-03 23:41:02 +0330

    New Commits

--- Exercism/armstrong_number.py
+++
@@ -1,8 +0,0 @@
-user_number = input("Number: ")
-length_of_user_number = len(user_number)
-
-
-for i in user_number:
-    new_user_number = int(i) ** length_of_user_number
-    
-print(new_user_number) 
