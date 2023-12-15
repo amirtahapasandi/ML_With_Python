2d077419d6b0ee8e64d1c5bd9162ea99ab6902dc
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2023-12-15 21:32:34 +0330

    New edit!

--- Partical projects with python/Convert_image_to_text/Convert_image_to_text/convert.py
+++ Partical projects with python/Convert_image_to_text/Convert_image_to_text/convert.py
@@ -4,3 +4,9 @@ import pytesseract
 import pathlib
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+text = ""
+# ans = input("Do you want the photos to be translated into Persian?(y/n): ")
+
+for path in pathlib.Path("Persian_picture").iterdir():
+    print()
