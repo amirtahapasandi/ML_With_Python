254c5134eabc708ca6f4b5808f2e8284d6978afa
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-01-04 21:14:03 +0330

    ...

--- main.py
+++ main.py
@@ -4,3 +4,7 @@
 # print(f"{text:#<20}")
 # print(f"{text:_>20}")
 # print(f"{text:.^20}")
+import csv
+
+with open("csv_file") as data_csv_file:
+    data_reader_csv_file = csv.reader(data_csv_file)
