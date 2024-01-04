a7da6b407d7f8b657dd9f53ad30d7828dd76f5f6
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-01-04 21:26:28 +0330

    New Commit

--- main.py
+++ main.py
@@ -4,10 +4,3 @@
 # print(f"{text:#<20}")
 # print(f"{text:_>20}")
 # print(f"{text:.^20}")
-import csv
-
-with open("csv_file.csv", "r") as data_csv_file:
-    data_reader_csv_file = csv.reader(data_csv_file)
-    for line in data_reader_csv_file:  
-        print(line[0])
-        
