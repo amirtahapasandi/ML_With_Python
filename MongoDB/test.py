7d046bd423a154d61f51763d5fec4f473c97d0f9
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-01-24 00:10:10 +0330

    .....

--- MongoDB/test.py
+++ MongoDB/test.py
@@ -1,3 +1,14 @@
-import pymongo as mongo
-import bson 
+import pymongo as pm
+import time 
 
+class MongoDB:
+    def __init__(self, DataBaseName:str = "local") -> None:
+        try:
+            self.Client = pm.MongoClient(host="127.0.0.1", port=27017)
+            self.DataBaseName = DataBaseName
+            self.DataBase = self.Client.get_database(self.DataBaseName)
+            self.Collections = self.DataBase.get_collection(self.DataBaseName)
+            print("Connected!")
+        except:
+            print("Not Connected!")
+        time.sleep(0.01)
