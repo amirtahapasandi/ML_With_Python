2ed274fdca1dee7f8eb7b90fb6925ec67db9443f
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-07-04 00:21:43 +0330

    ...

--- Homework(sabzlearn)/chapter 2/exercise6.py
+++ Homework(sabzlearn)/chapter 2/exercise6.py
@@ -3,5 +3,5 @@ second_number = int(input("Enter your second number: "))
 third_number = int(input("Enter your third number: "))
 
 sum_of_numbers = one_number + second_number + third_number
-result = sum_of_number // 3 
+result = sum_of_numbers // 3 
 print(result)
