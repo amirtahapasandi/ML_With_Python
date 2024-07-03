f8b1137f608446be4ed1bd8eef966dcf6a9ea086
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-07-04 00:15:34 +0330

    ...

---
+++ Homework(sabzlearn)/chapter 2/exercise5.py
@@ -0,0 +1,8 @@
+x = int(input("Enter the number: "))
+y = int(input("Enter the number: "))
+
+sum_of_numbers = x + y
+print(sum_of_numbers)
+minus_of_numbers = x - y
+print(minus_of_numbers)
+multiplication_of = 
