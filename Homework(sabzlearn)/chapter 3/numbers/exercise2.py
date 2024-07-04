1791b47a274bc1c8b8ce6ff520b0488d7cd9f441
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-07-04 14:51:33 +0330

    ...

--- Homework(sabzlearn)/chapter 3/numbers/exercise2.py
+++ Homework(sabzlearn)/chapter 3/numbers/exercise2.py
@@ -1,4 +1,4 @@
 number_of_user = int(input("Enter the number: "))
 
-print("square_of_number=", number_of_user ** 2)
-print("cube_of_number=", number_of_user ** 3)
+print("square_of_number =", number_of_user ** 2)
+print("cube_of_number =", number_of_user ** 3)
