2ac31dbe3c7fef18bebd35d8dc0f4a76aaf5f23d
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-07-04 19:50:16 +0330

    ...

--- Homework(sabzlearn)/chapter 3/string/exercise1.py
+++ Homework(sabzlearn)/chapter 3/string/exercise1.py
@@ -1,4 +1,3 @@
-import string
 str_of_user = input("str: ")
 
 count_of_sentence_in_str = str_of_user.count(".")
@@ -12,12 +11,3 @@ print(f"Count of word in str's user: {count_of_word_in_str + 1}")
 len_of_str = len(str_of_user)
 print(f"length of string's user: {len_of_str}")
 
-
-letters = string.ascii_lowercase and string.ascii_uppercase
-str_of_letters = str(letters)
-count = 0
-
-if str_of_letters in str_of_user:
-    count += 1
-    
-print(count)
