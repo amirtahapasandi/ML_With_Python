b5363e5f8c9d8a92091d34f1dac5c04db24257f7
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-07-04 18:16:21 +0330

    new edit

--- Homework(sabzlearn)/chapter 3/string/exercise1.py
+++ Homework(sabzlearn)/chapter 3/string/exercise1.py
@@ -8,3 +8,5 @@ count_of_word_in_str = str_of_user.count(" ")
 print(f"Count of word in str's user: {count_of_word_in_str + 1}")
 
 
+len_of_str = len(str_of_user)
+print(f"length of string's user: {len_of_str}")
