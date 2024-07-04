7736eaec5ceffbd4d4f32097950b5923ce5433bb
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-07-04 14:34:02 +0330

    ...

--- Homework(sabzlearn)/chapter 2/exercise4.py
+++ Homework(sabzlearn)/chapter 2/exercise4.py
@@ -1,9 +1,9 @@
 # Area of circle
 r = float(input("Enter the number: "))
 pi = 3.14
-area_of_circle = pi * (r ** 2) 
+area_of_circle = round(pi * (r ** 2)) 
 print(area_of_circle)
 
 # perimeter of the circle
-perimeter_of_circle = 2 * pi * r
+perimeter_of_circle = round(2 * pi * r)
 print(perimeter_of_circle)
