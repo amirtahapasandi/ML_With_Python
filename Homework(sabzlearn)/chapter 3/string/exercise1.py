1cff952ae21077c0a49fc0d1529a960b264f4c47
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-07-04 18:13:28 +0330

    ....

---
+++ Homework(sabzlearn)/chapter 3/string/exercise1.py
@@ -0,0 +1,10 @@
+str_of_user = input("str: ")
+
+count_of_sentence_in_str = str_of_user.count(".")
+print(f"Count of sentence in str's user: {count_of_sentence_in_str}")
+
+
+count_of_word_in_str = str_of_user.count(" ")
+print(f"Count of word in str's user: {count_of_word_in_str + 1}")
+
+
