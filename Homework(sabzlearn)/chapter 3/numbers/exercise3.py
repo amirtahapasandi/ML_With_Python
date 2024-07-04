d92aabd13716ca7bef6cb7e157576e110fad2da4
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-07-04 14:48:04 +0330

    ...

--- Homework(sabzlearn)/chapter 3/numbers/exercise3.py
+++ Homework(sabzlearn)/chapter 3/numbers/exercise3.py
@@ -1,5 +1,4 @@
 first_number = int(input("Enter the first number: "))
 second_number = int(input("Enter the second number: "))
 
-result = first_number ** second_number
-print(result)
+print("result=", first_number ** second_number)
