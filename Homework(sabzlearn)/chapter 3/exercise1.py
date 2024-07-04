fbad8ea8657b0fa4ab4bf4f9ed96ab80223f47a0
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-07-04 13:43:18 +0330

    ....

---
+++ Homework(sabzlearn)/chapter 3/exercise1.py
@@ -0,0 +1,9 @@
+# Area of circle
+r = int(input("Enter the number: "))
+pi = 3.14
+area_of_circle = pi * (r ** 2) 
+print(area_of_circle)
+
+# perimeter of the circle
+perimeter_of_circle = 2 * pi * r
+print(perimeter_of_circle)
