16c77dfeb82809d782465edc9b9dc6e17014bb95
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-07-04 13:39:09 +0330

    ...

--- datatype_test.py
+++ datatype_test.py
@@ -1,4 +1,14 @@
-x = {1, 2, 3, 4, 5}
-print(type(x))
-x = (1, 2, 3, 4, 5)
-print(type(x))
+# x = {1, 2, 3, 4, 5}
+# print(type(x))
+# x = (1, 2, 3, 4, 5)
+# print(type(x))
+
+x = int(input("Enter the number: "))
+temp = x % 10 
+print(temp)
+y = x // 1032
+temp2 = y % 10 
+print(temp2)
+z = y // 1032
+temp3 = z % 10 
+print(temp3)
