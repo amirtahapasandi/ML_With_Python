dcf6eb4c425ab276ebbe454f30ac66ce7fbd6671
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-07-05 19:17:58 +0330

    ...

--- Homework(sabzlearn)/copy.py
+++ Homework(sabzlearn)/copy.py
@@ -13,12 +13,12 @@ print(l2)
 
 
 # Deep copy
-l1 = [1, 2, ["A", "B"]]
-l2 = copy.deepcopy(l1)
+l3 = [1, 2, ["A", "B"]]
+l4 = copy.deepcopy(l3)
 
-print(id(l1))
-print(id(l2))
+print(id(l3))
+print(id(l4))
 
-l2[2][0] = "0"
-print(l1)
-print(l2)
+l4[2][0] = "0"
+print(l3)
+print(l4)
