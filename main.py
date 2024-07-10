b4076999be2a85e6f3097f7b91f2f724e1cccdd4
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-07-10 11:05:20 +0330

    ...

---
+++ main.py
@@ -0,0 +1,8 @@
+user_name = input("What's you're name? ")
+
+if user_name == "Amirtaha":
+    print("Hello my friend!")
+elif user_name == "Jadi":
+    print("Hello jadi, are you here? ")
+else:
+    print("Hello strange")
