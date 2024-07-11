50e6d499143ac0fc3225555ae53d193d3a37562e
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-07-11 11:04:09 +0330

    New edit

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 user_number = int(input("Enter some number: "))
+sum_of_user_number = user_number + user_number 
+count = 0
+
+
+while user_number != -1:
+    print(user_number)
+    count = count + 1
+
+print(count)
+print(sum_of_user_number)
