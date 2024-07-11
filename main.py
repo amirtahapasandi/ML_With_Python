528363804c4883fe958a7d10186dcc437164ae79
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-07-11 10:48:00 +0330

    ..

--- main.py
+++
@@ -1,8 +0,0 @@
-user_name = input("What's you're name? ")
-
-if user_name.capitalize() == "Amirtaha":
-    print("Hello my friend!")
-elif user_name.capitalize() == "Jadi":
-    print("Hello jadi, are you here? ")
-else:
-    print("Hello strange")
