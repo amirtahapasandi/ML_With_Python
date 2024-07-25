10f8cce6ae4a303c14b279bffc77cbd268b0ec64
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-07-25 22:31:12 +0330

    New Commit

--- Homework(sabzlearn)/chapter5/exercise2.py
+++ Homework(sabzlearn)/chapter5/exercise2.py
@@ -2,7 +2,7 @@ n1 = int(input("Num: "))
 n2 = int(input("Num: "))
 common_denominators = []
 
-for i in range(1,n1+1):
+for i in range(1, n1+1):
     if n1 % i == 0 and n2 % i == 0:
         common_denominators.append(i)
 print(common_denominators)
