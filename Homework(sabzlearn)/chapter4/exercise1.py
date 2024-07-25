f3893f48289dea818ca20721b49835947b538aea
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-07-25 14:41:26 +0330

    ....

--- Homework(sabzlearn)/chapter4/exercise1.py
+++ Homework(sabzlearn)/chapter4/exercise1.py
@@ -3,3 +3,5 @@ user_number = input("Enter your number: ")
 if user_number[-1] == "0" and "5":
     if int(user_number) % 2 == 0:
         print(f"{user_number} it is divisible by 2 and 5.")
+    else:
+        pritn(f"{user_number} it is not divisible by 2 or 5.")
