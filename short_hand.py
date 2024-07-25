f28fc685ffee7af868cf17c19ad91bd0bff91632
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-07-25 10:39:59 +0330

    New commit

--- short_hand.py
+++ short_hand.py
@@ -1,8 +1,10 @@
-# This technique is known as Ternary Operators, or Conditional Expressions.
-
 a = 2
 b = 330
 
 if a > b: print("a is greater than b")
 
 print("A") if a > b else print("B")
+
+# This technique is known as Ternary Operators, or Conditional Expressions.
+
+print("A") if a > b else print("=") if a == b else print("B")
