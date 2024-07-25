74b180fd26ee782e247b617c243fe5a6180304ef
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-07-25 21:32:14 +0330

    New Commit

---
+++ Homework(sabzlearn)/chapter5/exercise4.py
@@ -0,0 +1,8 @@
+n1 = int(input("Num: "))
+n2 = int(input("Num: "))
+common_denominators = []
+
+for i in range(1,9):
+    if n1 % i == 0 and n2 % i == 0:
+        common_denominators.append(i)
+print(min(common_denominators))
