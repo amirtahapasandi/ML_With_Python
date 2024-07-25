1efc85dd016c26c4063f4f1d6f4042b9d688e051
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-07-25 11:17:02 +0330

    New Commit

--- short_hand.py
+++ short_hand.py
@@ -8,3 +8,8 @@ print("A") if a > b else print("B")
 # This technique is known as Ternary Operators, or Conditional Expressions.
 
 print("A") if a > b else print("=") if a == b else print("B")
+
+# Get some input from the user and store it in a list.
+l = [int(x) for x in input("x,y,z: ").split(",")]
+print(30*"-")
+print(l)
