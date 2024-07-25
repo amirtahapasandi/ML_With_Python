14e78b621fe197e8e8a5047196e03c38179c24fc
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-07-25 22:25:18 +0330

    New Commit

--- Homework(sabzlearn)/chapter5/exercise1.py
+++ Homework(sabzlearn)/chapter5/exercise1.py
@@ -1,7 +1,14 @@
 n1 = int(input("Num:"))
 n2 = int(input("Num:"))
 
-min_num = n1 < n2 or n2 < n1
-max_num = n1 > n2 or n2 > n1
+if n1 < n2:
+    min_num = n1
+else:
+    min_num = n2
+if n1 > n2:
+    max_num = n1
+else:
+    mac_num = n2 
+
 for i in range(min_num, max_num):
     print(i)
