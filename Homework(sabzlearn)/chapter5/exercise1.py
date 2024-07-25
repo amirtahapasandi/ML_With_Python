288371919bcb6c817d030664b90eefd630ba6032
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-07-25 22:20:05 +0330

    ...

--- Homework(sabzlearn)/chapter5/exercise1.py
+++ Homework(sabzlearn)/chapter5/exercise1.py
@@ -1,5 +1,7 @@
 n1 = int(input("Num:"))
 n2 = int(input("Num:"))
 
-for i in range(n1, n2):
+min_num = n1 < n2 or n2 < n1
+max_num = n1 > n2 or n2 > n1
+for i in range(min_num, max_num):
     print(i)
