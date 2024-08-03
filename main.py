ab6581e026cd71a4a3a343434d87def201210f49
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-08-03 10:23:09 +0330

    New commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import request as req
+
+query : str = input("Enter your question: ")
+token : str = "703612:669d0ed0dec8a"
+URL : str = f"https://one-api.ir/chatgpt/?token={token}&action=gpt4o&q={query}"
+response : dict = req.get(URL).json()
+result = response["result"]
+
+with open("response.txt", "W") as f:
+    f.write(result)
+print("Wrote to response.txt!")
