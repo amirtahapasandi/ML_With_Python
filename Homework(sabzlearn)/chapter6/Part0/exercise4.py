f611390783df7870016320ed37d9d91918e7fb3f
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-08-07 13:47:55 +0330

    New Commit

--- Homework(sabzlearn)/chapter6/Part0/exercise4.py
+++ Homework(sabzlearn)/chapter6/Part0/exercise4.py
@@ -1,6 +1,7 @@
-number = int(input("Num: "))
+def square_number(number):
+    for i in range(1,10):
+        power_of_numbers = i * i
+        if number == power_of_numbers:
+            return f"{number} is a square number!"
 
-for i in range(1,10):
-    power_of_numbers = i * i
-    if number == power_of_numbers:
-        print(f"{number} is a square number!")
+print(square_number(number = int(input("Num: "))))
