3135d2916126bebbd186436be6953ca1a9f33234
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-08-08 16:30:14 +0330

    New commit

--- Homework(sabzlearn)/chapter6/Part0/exercise3.py
+++ Homework(sabzlearn)/chapter6/Part0/exercise3.py
@@ -1,4 +1,7 @@
-def user_sum(x:int,y:int)-> int:
-    return x + y
+def user_sum(iterable):
+    sum_of_numbers = 0
+    for i in iterable:
+        sum_of_numbers += i
+    return sum_of_numbers
 
-print(user_sum(x=int(input("Num:")),y=int(input("Num:"))))
+print(user_sum([1,2,3,4,5,6,7,8,9,10]))
