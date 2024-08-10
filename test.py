cb358c5415b181ce83c6337fc0c1372fc65bcf0f
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-08-10 10:58:41 +0330

    New Commit

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from requests import get
 from csv import writer
-from datetime import date,time
+from datetime import datetime, date
 
 token : str = "703612:669d0ed0dec8a"
 URL : str = f"https://one-api.ir/DigitalCurrency/?token={token}"
@@ -15,7 +15,7 @@ price_of_tether = tether["price"]
 binancecoin = result[3]
 price_of_binancecoin = binancecoin["price"]
 date = date.today()
-time = time()
+time = datetime.now().time()
 
 with open("csv_file.csv", "w", newline = "") as cf:
     writer = writer(cf)
