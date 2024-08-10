f2f83abbef3ac3fcfe170265c9668f21cdf61d34
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-08-10 10:20:46 +0330

    New Commit

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from requests import get
 from csv import writer
-from datetime import date
+from datetime import date,time
 
 token : str = "703612:669d0ed0dec8a"
 URL : str = f"https://one-api.ir/DigitalCurrency/?token={token}"
@@ -14,9 +14,14 @@ tether = result[2]
 price_of_tether = tether["price"]
 binancecoin = result[3]
 price_of_binancecoin = binancecoin["price"]
-
+date = date.today()
+time = time()
 
 with open("my_csv_file.csv", "w") as mcf:
     writer = csv.writer(mcf)
     writer.writerow(["name", "time", "date", "price"])
-    data = [["bitcoin", ],[],[],[]]
+    data = [["bitcoin", f"{time}", f"{date}", f"{price_of_bitcoin}"],
+            ["etherum", f"{time}", f"{date}", f"{price_of_etherum}"],
+            ["tether", f"{time}", f"{date}", f"{price_of_tether}"],
+            ["binancecoin", f"{time}", f"{date}", f"{price_of_binancecoin}"]]
+    writer.writerows(data)
