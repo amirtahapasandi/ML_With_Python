cd16259da0ec541697ed0d7d5206efd7456b26a4
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-08-10 10:03:31 +0330

    New Commit

---
+++ test.py
@@ -0,0 +1,25 @@
+from requests import get
+import csv
+
+token : str = "703612:669d0ed0dec8a"
+URL : str = f"https://one-api.ir/DigitalCurrency/?token={token}"
+response : dict = get(URL).json()
+result = response["result"]
+bitcoin = result[0]
+price_of_bitcoin = bitcoin["price"]
+ethereum = result[1]
+price_of_ethereum = ethereum["price"]
+tether = result[2]
+price_of_tether = tether["price"]
+binancecoin = result[3]
+price_of_binancecoin = binancecoin["price"]
+
+print(price_of_bitcoin)
+print(price_of_ethereum)
+print(price_of_tether)
+print(price_of_binancecoin)
+
+with open("my_csv_file.csv", "w") as mcf:
+    writer = csv.writer(mcf)
+    writer.writerow(["name", "time", "date", "price"])
+    data = [["bitcoin", ],[],[],[]]
