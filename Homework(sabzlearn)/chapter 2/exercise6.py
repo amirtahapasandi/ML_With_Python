31c6ca42c0f48c0e42a25ae982df21c55eef072a
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-08-24 20:44:02 +0330

    New Commit

--- Homework(sabzlearn)/chapter 2/exercise6.py
+++
@@ -1,7 +0,0 @@
-one_number = int(input("Enter your first number: "))
-second_number = int(input("Enter your second number: "))
-third_number = int(input("Enter your third number: "))
-
-sum_of_numbers = one_number + second_number + third_number
-result = sum_of_numbers // 3 
-print(result)
