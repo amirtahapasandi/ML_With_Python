448f3cb7518b423f791f4062068061dd128358ff
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-08-24 20:44:34 +0330

    New Commit

--- Homework(sabzlearn)/chapter 3/numbers/exercise4.py
+++
@@ -1,5 +0,0 @@
-one_number = int(input("Enter your first number: "))
-second_number = int(input("Enter your second number: "))
-third_number = int(input("Enter your third number: "))
-
-print("result =", round((one_number + second_number + third_number) / 3, 2))
