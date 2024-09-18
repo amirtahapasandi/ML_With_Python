c77d6b9b52713409bdacc01e6a3a772a08797201
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-09-18 15:25:12 +0330

    Fix the bugs in problem_1

--- Project Euler/Problem_1.py
+++ Project Euler/Problem_1.py
@@ -2,14 +2,16 @@
 
 sum_of_all_the_multiples_of_three = 0
 for i in range(1,1001):
-    sum_of_all_the_multiples_of_three = sum_of_all_the_multiples_of_three + (3*i)
+    if i % 3 == 0:
+        sum_of_all_the_multiples_of_three += i
 print(f"sum of all the multiples of three: {sum_of_all_the_multiples_of_three}")
 
 print(90 * "-")
 
 sum_of_all_the_multiples_of_five = 0
 for j in range(1,1001):
-    sum_of_all_the_multiples_of_five = sum_of_all_the_multiples_of_five + (5*i)
+    if j % 5 == 0:
+        sum_of_all_the_multiples_of_five += j
 print(f"Sum of all the multiples of five: {sum_of_all_the_multiples_of_five}")
 
 print(90 * "-")
