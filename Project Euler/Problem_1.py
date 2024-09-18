78ddc8e632503976b9128484c063355c3fe32879
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-09-18 15:32:28 +0330

    Fix the another bug in my code.

--- Project Euler/Problem_1.py
+++ Project Euler/Problem_1.py
@@ -1,20 +1,7 @@
 # Multiples of 3 or 5
 
-sum_of_all_the_multiples_of_three = 0
+sum_of_all_the_multiples_of_three_and_five = 0
 for i in range(1,1001):
-    if i % 3 == 0:
-        sum_of_all_the_multiples_of_three += i
-print(f"sum of all the multiples of three: {sum_of_all_the_multiples_of_three}")
-
-print(90 * "-")
-
-sum_of_all_the_multiples_of_five = 0
-for j in range(1,1001):
-    if j % 5 == 0:
-        sum_of_all_the_multiples_of_five += j
-print(f"Sum of all the multiples of five: {sum_of_all_the_multiples_of_five}")
-
-print(90 * "-")
-
-sum_of_all_the_multiples_of_three_and_five = sum_of_all_the_multiples_of_three + sum_of_all_the_multiples_of_five
-print(f"Sum of all the multiples of three and five: {sum_of_all_the_multiples_of_three_and_five}")
+    if i % 3 == 0 or i % 5 == 0:
+        sum_of_all_the_multiples_of_three_and_five += i
+print(f"sum_of_all_the_multiples_of_three_and_five: {sum_of_all_the_multiples_of_three_and_five}")
