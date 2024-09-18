fd404c25d9ee1a307ca2cf1aab64d2394b980a50
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-09-18 15:34:29 +0330

    Fix the another buggg in my fucking code.

--- Project Euler/Problem_1.py
+++ Project Euler/Problem_1.py
@@ -1,7 +1,7 @@
 # Multiples of 3 or 5
 
 sum_of_all_the_multiples_of_three_and_five = 0
-for i in range(1,1001):
+for i in range(1,1000):
     if i % 3 == 0 or i % 5 == 0:
         sum_of_all_the_multiples_of_three_and_five += i
 print(f"sum_of_all_the_multiples_of_three_and_five: {sum_of_all_the_multiples_of_three_and_five}")
