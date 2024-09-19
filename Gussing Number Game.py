422caf86a5bbe3ede138178979f152cf0f4b616f
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-09-19 14:14:15 +0330

    Fix the bugs in gussing number game.

--- Gussing Number Game.py
+++ Gussing Number Game.py
@@ -2,13 +2,14 @@ from random import randint
 
 random_number = randint(1,100)
 while True:
-    user_choice = input("Guss the number between 1 and 100: ")
-    if int(user_choice) > random_number:
-        print("Too high!")
-    elif int(user_choice) < random_number:
-        print("Too low!")
-    elif int(user_choice) == random_number:
-        print("Congratulations! You gussed the number.")
-        break
-    else:
-        print("Enter a valid number!")
+    try:
+        user_choice = int(input("Guss the number between 1 and 100: "))
+        if user_choice > random_number:
+            print("Too high!")
+        elif user_choice < random_number:
+            print("Too low!")
+        elif user_choice == random_number:
+            print("Congratulations! You gussed the number.")
+            break
+    except ValueError:
+        print("Please enter a valid number.")
