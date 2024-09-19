8e7e658e9818a2e0ce5b83b534dd1d0c2186bc42
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-09-19 13:57:01 +0330

    Make a another fun game.

---
+++ Gussing Number Game.py
@@ -0,0 +1,12 @@
+from random import randint
+
+random_number = randint(1,100)
+while True:
+    user_choice = int(input("Guss the number between 1 and 100: "))
+    if user_choice > random_number:
+        print("Too high!")
+    elif user_choice < random_number:
+        print("Too low!")
+    elif user_choice == random_number:
+        print("Congratulations! You gussed the number.")
+        break
