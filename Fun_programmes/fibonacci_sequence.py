e3498c64aeb29a9b634c3fcacd40ff5c6a315a44
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-11-18 16:37:44 +0330

    new commit,.

--- Fun_programmes/fibonacci_sequence.py
+++
@@ -1,8 +0,0 @@
-i = 0
-first_number,second_number = 0,1
-last_sentence = int(input("Enter last sentence(number): "))
-
-while i <= last_sentence:
-    first_number, second_number = second_number, first_number + second_number
-    print(second_number)
-    i += 1
